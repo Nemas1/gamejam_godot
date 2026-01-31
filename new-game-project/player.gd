16aaa4aef1f5815003dd9d1a2948da2c098fc156
Nemas1 <140882911+Nemas1@users.noreply.github.com>
2026-01-31 13:42:59 +0200

    bunc of shit added

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@export var inventory: Inventory
 
 var speed = 200 
 
@@ -8,6 +9,11 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	
+	animation_handler(direction)
+	move_and_slide()
+	
+
+func animation_handler(direction: Vector2):
 	if direction.x > 0:
 		animated_sprite_2d.play("walk_right")
 	elif direction.x < 0:
@@ -18,6 +24,3 @@ func _physics_process(delta):
 		animated_sprite_2d.play("walk_up")
 	else:
 		animated_sprite_2d.play("idle")
-	
-	move_and_slide()
-	
