e17845146ae81172d485f5e92085c0d99288ecd7
Nemas1 <140882911+Nemas1@users.noreply.github.com>
2026-01-31 11:30:57 +0200

    character_anims

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,9 +1,23 @@
 extends CharacterBody2D
 
-var speed = 400 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+var speed = 200 
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-
+	
+	if direction.x > 0:
+		animated_sprite_2d.play("walk_right")
+	elif direction.x < 0:
+		animated_sprite_2d.play("walk_left")
+	elif direction.y > 0:
+		animated_sprite_2d.play("walk_down")
+	elif direction.y < 0:
+		animated_sprite_2d.play("walk_up")
+	else:
+		animated_sprite_2d.play("idle")
+	
 	move_and_slide()
+	
